43fd2db18754ca943be4b69207d8a77bec67a5a8
tayyaba-ramzan <tayyabaramzan.pak@gmail.com>
2025-08-03 02:15:26 +0500

    Explored and understood agent handoff mechanism in Agentic AI

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from agents import Runner,set_tracing_disabled
-from my_agents.math_agent import first_agent
+from my_agents.math_agent import my_assistant
 
 set_tracing_disabled(True)
 
-res=Runner.run_sync(starting_agent=first_agent,input="2*5=?",max_turns=4)
+res=Runner.run_sync(starting_agent=my_assistant,input="hello!")
 
+print(res.last_agent)
 print(res.final_output)
--- my_agents/math_agent.py
+++ my_agents/math_agent.py
@@ -4,9 +4,38 @@ from my_data_type.my_data_type_schema import MyData
 from my_tools.math_tool import add,sub,mul,div
 from my_tools.user_data_tool import fetch_user_data,fetch_user_dat_by_id
 from agents.agent import StopAtTools
+from agents.extensions.handoff_prompt import RECOMMENDED_PROMPT_PREFIX
+
+math_agent=Agent(
+    name="math-agent",
+    instructions=f"""{RECOMMENDED_PROMPT_PREFIX}
+    you are a helpfull for math assistant
+""",
+    model=model,
+    handoff_description="This is a math teacher"
+)
+
+english_agent=Agent(
+    name="english-agent",
+    instructions=f"""{RECOMMENDED_PROMPT_PREFIX}
+    you are a helpfull for english assistant
+""",
+    model=model,
+    handoff_description="This is a english teacher"
+)
+
+my_assistant=Agent(
+    name="my-assistant",
+    instructions="you are a helfull assistant",
+    model=model,
+    handoffs=[math_agent,english_agent]
+)
+
+# for ag in my_assistant.handoffs:
+#     print(f"{ag.name}: {ag.instructions}")
 
 
-first_agent=Agent(name="math-agent",instructions="you are helpfull for math calculations",model=model,tools=[add,sub,mul,div],tool_use_behavior=StopAtTools(stop_at_tool_names=["add","mul"]),model_settings=ModelSettings(tool_choice="sub",parallel_tool_calls=False),reset_tool_choice=True)
+# first_agent=Agent(name="math-agent",instructions="you are helpfull for math calculations",model=model,tools=[add,sub,mul,div],tool_use_behavior=StopAtTools(stop_at_tool_names=["add","mul"]),model_settings=ModelSettings(tool_choice="sub",parallel_tool_calls=False),reset_tool_choice=True)
 
 # last_agent=Agent(name="english-agent",instructions="you are helpfull assistant",model=model,tools=[first_agent.as_tool(tool_name="first-agent",tool_description="This is a math assistant tool")])
 
