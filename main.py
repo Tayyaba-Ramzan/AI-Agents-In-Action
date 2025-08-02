cf9fde6d329ce5a7d7a910457066522637dfefa9
tayyaba-ramzan <tayyabaramzan.pak@gmail.com>
2025-08-02 17:09:49 +0500

    Completed agent setup with integrated function calling

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from my_agents.math_agent import agent
 
 set_tracing_disabled(True)
 
-res=Runner.run_sync(starting_agent=agent,input="mujhy user ka data id chihe is ki id 1 ho")
+res=Runner.run_sync(starting_agent=agent,input="mujhy user ka data id chihe is ki id 1,6,9 ho")
 
 print(res.final_output)
